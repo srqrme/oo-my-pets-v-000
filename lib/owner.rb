d93fa146710e8a04cedcb15c71ee222167e30c80
Sharmaine Requerme <srqrme@gmail.com>
2018-08-15 14:44:58 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -57,7 +57,7 @@ class Owner
   end
 
   def sell_pets
-    pets.each do |species, animals|
+    @pets.each do |species, animals|
       animals.each do |animal|
         animal.mood = "nervous"
       end
