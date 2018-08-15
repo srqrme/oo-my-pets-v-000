c499b9f893bbcead9ddad793cdd7f343f19419a2
Sharmaine Requerme <srqrme@gmail.com>
2018-08-15 13:39:18 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
 
-  attr_accessor :mood
-  attr_reader :name
+  attr_accessor :mood # Dog's mood can be read and changed
+  attr_reader :name # Dog's name can only be read, but not changed
   # code goes here
   def initialize(name)
-    @name = name
-    @mood = "nervous"
+    @name = name # Dog is initialized with a name
+    @mood = "nervous" # Dog is initialized with a nervous mood
   end
 end
