faa2e209ea2acf0260f8eddb14e6b08837bd82e4
Sharmaine Requerme <srqrme@gmail.com>
2018-08-15 13:59:19 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,15 +1,3 @@
 class Owner
   # code goes here
-
-  @@all = []
-
-  def initialize(owner)
-    @@all << self
-  end
-
-  def self.all
-    @@all.each do |o|
-      puts o.owner
-    end
-  end
 end
